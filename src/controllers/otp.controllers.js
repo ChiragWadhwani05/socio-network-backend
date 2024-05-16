8abb7f5317e97e7f6b59fd636a01f41645391733
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2024-05-16 12:12:27 +0530

    refactor(otp): Improve the registerOTP content

--- src/controllers/otp.controllers.js
+++ src/controllers/otp.controllers.js
@@ -2,6 +2,8 @@ import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { OTP } from "../models/otp.models.js";
 import { sendMail } from "../utils/mail.js";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/apiError.js";
 
 function generateOtp() {
   return Math.floor(100000 + Math.random() * 900000);
@@ -10,9 +12,35 @@ function generateOtp() {
 const registerOTP = asyncHandler(async (req, res) => {
   const { email } = req.body;
 
+  const existedUser = await User.findOne({ email });
+
+  if (existedUser) {
+    throw new ApiError(409, "User already exists.");
+  }
+
   const otp = generateOtp();
 
-  sendMail({ email, subject: "OTP", content: `OTP: ${otp}` });
+  sendMail({
+    email,
+    subject: `${otp} is your Sanga Verification Code`,
+    content: `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Your Sanga Verification Code</title>
+    </head>
+    <body>
+        <p>Thank you for choosing Sanga! Your One-Time Password (OTP) for verification is:</p>
+        <div style="background-color: #f0f0f0; padding: 10px; border-radius: 5px; margin-bottom: 10px;">
+            <strong style="font-size: 18px;" id="otp">${otp}</strong>
+        </div>
+        <p>Please use this code to complete the verification process.</p>
+        <p>If you have any questions or need further assistance, feel free to reach out to our support team.</p>
+        <p>Best regards,<br/>The Sanga Team</p>
+    </body>
+    </html>
+    `,
+  });
 
   await OTP.create({ email, otp });
 
